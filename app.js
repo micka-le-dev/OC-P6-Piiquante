fbcc134b9a007002faef3219b2e88feead4b5846
micka-le-dev <mickael.petithory.pro@gmail.com>
2022-12-30 09:59:35 +0100

    ajoute la connexion à mongoDB et gére l'erreur CORS

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+/** import des routes */
 const routerParDefaut = require('./routes/defaut')
+
+
+mongoose.set('strictQuery', true)
+mongoose.connect('mongodb+srv://go-fullstack-v3-fr:QCwY6hhLnLxTl1vN@oc-p6-cours--go-fullsta.gnfmyuh.mongodb.net/?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'))
+
+
+
+
 const app = express()
+app.use(express.json()) // parse l'objet requète
+
+
+/** résoud l'erreur CORS */
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+    next()
+})
+
+
 
 app.use(routerParDefaut)
 
